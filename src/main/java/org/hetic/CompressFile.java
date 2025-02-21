1b71808c25ecf74e1b2dd86ea59106ac09cac3da
Julien Huynh <julienhynh@gmail.com>
2025-02-21 11:51:07 +0100

    add main args

--- src/main/java/org/hetic/CompressFile.java
+++ src/main/java/org/hetic/CompressFile.java
@@ -18,30 +18,34 @@ public class CompressFile {
             CompressedChunkStorageSystem storageSystem = new CompressedChunkStorageSystem();
             FileReconstructor reconstructor = new FileReconstructor(storageSystem);
 
-            // // 2. Traiter les fichiers
-            String folderPath = "data-files";
-            System.out.println("Traitement des fichiers...\n");
-            long startTime = System.currentTimeMillis();
-            processFolder(storageSystem, folderPath);
-            long endTime = System.currentTimeMillis();
-            System.out.println("\nTraitement terminé en " + (endTime - startTime) + " ms");
+            // Tests de performance de compression
+            if (args[0] != null && args[0].equals("compare")) {
+                runCompressionBenchmark("data-files");
+            } else {
+                // // 2. Traiter les fichiers
+                String folderPath = "data-files";
+                System.out.println("Traitement des fichiers...\n");
+                long startTime = System.currentTimeMillis();
+                processFolder(storageSystem, folderPath);
+                long endTime = System.currentTimeMillis();
+                System.out.println("\nTraitement terminé en " + (endTime - startTime) + " ms");
 
-            // 2.1 Tests de performance de compression
-            // runCompressionBenchmark("data-files");
+                // // 3. Afficher les statistiques de déduplication et de compression
+                storageSystem.printChunkDetails();
+                DeduplicationStats stats = storageSystem.calculateDetailedStats();
+                System.out.println(stats.toString());
+                storageSystem.printCompressionStats();
 
-            // // 3. Afficher les statistiques de déduplication et de compression
-            storageSystem.printChunkDetails();
-            DeduplicationStats stats = storageSystem.calculateDetailedStats();
-            System.out.println(stats.toString());
-            storageSystem.printCompressionStats();
+                // // 5. Reconstruire un fichier spécifique
+                if (args[1] != null) {
+                    // // 4. Afficher les fichiers disponibles pour reconstruction
+                    reconstructor.listAvailableFiles();
 
-            // // 4. Afficher les fichiers disponibles pour reconstruction
-            reconstructor.listAvailableFiles();
-
-            // // 5. Reconstruire un fichier spécifique
-            String fileToReconstruct = "text1.txt";
-            System.out.println("\nReconstruction du fichier : " + fileToReconstruct);
-            reconstructor.reconstructFile(fileToReconstruct);
+                    String fileToReconstruct = args[1];
+                    System.out.println("\nReconstruction du fichier : " + fileToReconstruct);
+                    reconstructor.reconstructFile(fileToReconstruct);
+                }
+            }
 
         } catch (Exception e) {
             System.err.println("Erreur inattendue : " + e.getMessage());
